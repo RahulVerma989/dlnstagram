fca3721a6a871d3ab9d73f17966d5be84555b62a
RahulVerma989 <RahulVerma989@users.noreply.github.com>
2023-03-28 23:44:44 +0530

    creating qa branch

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-port : 2024,
-brandName: "dlnstagram",
-domainName: "dlnstagram.com",
-homePage: "https://www.dlnstagram.com/",
-cdnrail: "cdnrail.com"
-};
--- scripts/start_application.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /home/ec2-user/dlnstagram
-pm2 start app.js
