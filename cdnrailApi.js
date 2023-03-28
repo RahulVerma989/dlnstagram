a5b1282c5b58cf8b64354d2fd64e742ad04bdeb3
RahulVerma989 <RahulVerma989@users.noreply.github.com>
2023-03-28 23:45:50 +0530

    adding app code to qa

---
+++ cdnrailApi.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const config = require('./config');
+
+const cdnrailApi = {
+  // This function takes in an object with 'mediaUrl', and returns a Promise
+  // that resolves to the URL of the media file on cdnrail.com.
+  getMediaUrl: (mediaUrl) => {
+    const cdnrailUrl = `http://${config.cdnrail}/api/v1/media?url=${mediaUrl}`;
+    console.log("Making request to cdnrail using url: " + cdnrailUrl);
+    return axios.get(cdnrailUrl)
+      .then(response => {
+        // Extract the media URL from the response and return it.
+        console.log("Got response from cdnrail:");
+        console.log(response.data);
+        return response.data;
+      })
+      .catch(error => {
+        // If an error occurs, return a rejected Promise with the error message.
+        console.log("Got an error in getMediaUrl(): "+ error.message);
+        return Promise.reject(error.message);
+      });
+  }
+};
+
+module.exports = cdnrailApi;
