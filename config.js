1b05f89d74e943bb3558e4649a13d2d2f9798ce3
RahulVerma989 <RahulVerma989@users.noreply.github.com>
2023-04-02 19:15:14 +0530

    cleaning the branch

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-port : 2024,
-brandName: "dlnstagram",
-domainName: "dlnstagram.com",
-homePage: "https://www.dlnstagram.com/",
-cdnrail: "cdnrail.com",
-supportEmail: "dlnstagram99@gmail.com"
-};
